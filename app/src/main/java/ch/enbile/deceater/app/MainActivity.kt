7787dd5798ea013d3704987fa745e7e736d5a56f
Marco Endres <marco.endres@clavisit.com>
2020-10-14 16:31:22 +0200

    #1 Added button for darktheme and added intent for logout

--- app/src/main/java/ch/enbile/deceater/app/MainActivity.kt
+++ app/src/main/java/ch/enbile/deceater/app/MainActivity.kt
@@ -1,11 +1,22 @@
 package ch.enbile.deceater.app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import ch.enbile.deceater.app.ui.login.LoginActivity
+import ch.enbile.deceater.app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logOutButton = findViewById<Button>(R.id.logout)
+        logOutButton.setOnClickListener {
+            val myIntent = Intent(this@MainActivity, LoginActivity::class.java)
+            this@MainActivity.startActivity(myIntent)
+        }
     }
 }
