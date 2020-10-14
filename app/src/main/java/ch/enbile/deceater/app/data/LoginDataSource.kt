defa48bab5aa2b9886f9aafa6dea883fb5db41e1
chrigiBisig <christian.bisig@sonova.com>
2020-10-14 17:57:39 +0200

    implement authentication logic

--- app/src/main/java/ch/enbile/deceater/app/data/LoginDataSource.kt
+++ app/src/main/java/ch/enbile/deceater/app/data/LoginDataSource.kt
@@ -1,18 +1,38 @@
 package ch.enbile.deceater.app.data
 
 import ch.enbile.deceater.app.data.model.LoggedInUser
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.squareup.okhttp.*
 import java.io.IOException
+import java.lang.reflect.Type
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
-class LoginDataSource {
+class LoginDataSource(var httpClient: OkHttpClient = OkHttpClient()) {
+    val url = "https://enbile.westeurope.cloudapp.azure.com/deceater/api/user";
 
     fun login(username: String, password: String): Result<LoggedInUser> {
         try {
-            // TODO: handle loggedInUser authentication
-            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
-            return Result.Success(fakeUser)
+            var credential = Credentials.basic(username, password)
+
+            val request: Request = Request.Builder()
+                .header("Authorization", "your token")
+                .url("$url/authenticate")
+                .addHeader("Authorization", credential)
+                .build()
+
+                val response = httpClient.newCall(request).execute()
+
+                if(response.isSuccessful) {
+                    val json = response.body().string()
+                    val type: Type = object : TypeToken<LoggedInUser?>() {}.type
+                    val user : LoggedInUser = Gson().fromJson(json, type)
+                    return Result.Success(user)
+                }
+
+            return Result.Error(IOException("Authentication failed"))
         } catch (e: Throwable) {
             return Result.Error(IOException("Error logging in", e))
         }
--- app/src/main/java/ch/enbile/deceater/app/data/model/LoggedInUser.kt
+++ app/src/main/java/ch/enbile/deceater/app/data/model/LoggedInUser.kt
@@ -4,6 +4,10 @@ package ch.enbile.deceater.app.data.model
  * Data class that captures user information for logged in users retrieved from LoginRepository
  */
 data class LoggedInUser(
-        val userId: String,
-        val displayName: String
+        var username : String,
+        var firstname: String,
+        var lastname: String,
+        var password: String,
+        var enabled : Boolean = true,
+        var roles : Set<Role> = hashSetOf()
 )
