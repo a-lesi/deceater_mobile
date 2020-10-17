995d5ed00fe7c6eed2808ae6eb257aea7e9583e1
Armend Lesi <armend.lesi@gmail.com>
2020-10-17 11:19:33 +0200

    add simple notification without timer and data

--- app/src/main/java/ch/enbile/deceater/app/MainActivity.kt
+++ app/src/main/java/ch/enbile/deceater/app/MainActivity.kt
@@ -1,12 +1,18 @@
 package ch.enbile.deceater.app
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import ch.enbile.deceater.app.ui.login.LoginActivity
 import ch.enbile.deceater.app.R
 
@@ -50,7 +56,29 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        val CID = "Deceater_Channel"
+        val CNAME = "Deceater Notifications"
+        val CDESC = "Ein Channel für Deceater "
+        val CIMP = NotificationManager.IMPORTANCE_HIGH
+        var notificationId = 1
+        val manager: NotificationManagerCompat = NotificationManagerCompat.from(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel: NotificationChannel = NotificationChannel(CID, CNAME, CIMP)
+            channel.description = CDESC
+            manager.createNotificationChannel(channel)
+        }
+
+        val notification: Notification
+        notification = NotificationCompat.Builder(this, CID)
+            .setSmallIcon(R.drawable.ic_stat_deceater)
+            .setContentTitle("Deceater")
+            .setContentText("You are eating ... today!")
+            .build()
+        manager.notify(notificationId++, notification)
     }
+
     private fun reset() {
         val intent = Intent(this@MainActivity, MainActivity::class.java)
         startActivity(intent)
